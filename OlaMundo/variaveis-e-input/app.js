36348b35db79f81351301442998217358a00173c
cmagsilva <99703967+cmagsilva@users.noreply.github.com>
2022-02-25 18:00:11 -0300

    Criação de variavel

--- OlaMundo/variaveis-e-input/app.js
+++ OlaMundo/variaveis-e-input/app.js
@@ -1,11 +1,13 @@
-const readline = require('readline').createInterface({
-    imput: process.stdin,
+const readLine = require('readline').createInterface({
+    input: process.stdin,
     output: process.stdout
-})
+});
 
 const minhaPrimeiraConstanteString = 'Minha primeira constante';
 console.log(minhaPrimeiraConstanteString);
 
 let leituraDeCampo;
-readline.question('Por favor digite alguma coisa', input => leituraDeCampo = input);
-console.log( 'o usuario digitou:'(leituraDeCampo));
+readLine.question('Informe sua Idade:', input => { 
+    leituraDeCampo = input;
+    console.log( `o usuario digtou: ${leituraDeCampo}`);
+});
